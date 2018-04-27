97c7808d85b6dc514a981d9193b5e8751a105734
heydenjq <jonas.heyden@informatik.hu-berlin.de>
2018-04-27 12:07:55 +0200

    first commit

---
+++ blatt1/hello.py
@@ -0,0 +1,6 @@
+def main():
+    name = raw_input("Hello, what's your name? ")
+    print "Hello " + name
+
+if __name__ == "__main__":                                                      
+        main()
